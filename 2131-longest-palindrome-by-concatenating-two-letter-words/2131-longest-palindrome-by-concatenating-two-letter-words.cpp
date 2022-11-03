5dfdf293d91251acbcda8c8e688e9d3758267385
SHIVAM KUMAR <57912926+Shivamk4090@users.noreply.github.com>
2022-11-04 01:30:28 +0530

    Time: 739 ms (58.56%), Space: 167.9 MB (82.62%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -5,19 +5,20 @@ public:
         unordered_map<string, int> mp;
         for(auto e : words) mp[e]++;
         
-        int single = 0, odd = 0;
+        bool odd = 0;
         
         for(auto e:mp){
             string rs = "";
             rs +=  e.first[1];
             rs +=  e.first[0];
             if(e.first==rs && mp[e.first]%2!=0){
-                odd++;
-                if(odd>1){
+                if(odd==1){
                     ans += 2*(mp[e.first]-1);
                 }else{
                     ans += 2*(mp[e.first]);
-                }            
+                }
+                odd = 1;
+
             }
             else if (mp.find(rs)!=mp.end()){
                 ans += 2*min(mp[e.first], mp[rs]);
