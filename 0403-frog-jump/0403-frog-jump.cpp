8fbb412d4d10320b9f265eb0fc32e96ff52df2fa
SHIVAM KUMAR <57912926+Shivamk4090@users.noreply.github.com>
2022-11-13 00:08:57 +0530

    Time: 550 ms (24.86%), Space: 38.1 MB (57.33%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -3,21 +3,16 @@ class Solution
 public:
     bool canCross(vector<int> &s)
     {
-
         int n = s.size();
         vector<int> mem(n, false);
-        vector<set<int>> mpj(n);
+        vector<unordered_set<int>> mpj(n);
         mpj[0] = {0};
         mem[0] = true;
         int c = 0;
-        for (auto i : s)
-        {
-            for (auto j : mpj[c])
-            {
-                for (int k = -1; k <= 1; k++)
-                {
-                    if (j + k > 0 && binary_search(s.begin(), s.end(), i+j+k))
-                    {
+        for (auto i : s){
+            for (auto j : mpj[c]){
+                for (int k = -1; k <= 1; k++){
+                    if (j + k > 0 && binary_search(s.begin(), s.end(), i+j+k)){
                         int idx = lower_bound(s.begin(), s.end(), i+j+k) - s.begin();
                         mem[idx] = true;
                         mpj[idx].insert(j + k);
