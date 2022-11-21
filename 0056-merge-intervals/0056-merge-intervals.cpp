2192cdb337c5c4b99033e986d6735ebcf0fb7833
SHIVAM KUMAR <57912926+Shivamk4090@users.noreply.github.com>
2022-11-21 19:48:18 +0530

    Time: 50 ms (82.01%), Space: 19.2 MB (40.69%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& v) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        vector<vector<int>> ans;
+        sort(v.begin(), v.end());
+        vector<int> temp = v[0];
+        //brute force
+        for(int i=1; i<v.size(); i++){
+            // merge interval
+            if(v[i][0]<= temp[1]){
+                temp[1] = max(temp[1], v[i][1]);
+            }else{
+                //cuurent interval can't be merged with the temp
+                ans.push_back(temp);
+                temp = v[i];
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+    }
+};
