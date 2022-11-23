6ca5f2f69a08b87e483e76f7dfa0c3e26ba5f45d
SHIVAM KUMAR <57912926+Shivamk4090@users.noreply.github.com>
2022-11-23 17:54:57 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int mid, int r)
+    {
+         // Your code here
+        int* ans =  new int[r-l+1];
+        int i = l, j = mid+1;
+        int p = 0;
+        while(i<=mid && j<=r){
+            if(arr[i]<=arr[j]){
+                ans[p]= arr[i];
+                i++;
+                p++;
+            }else{
+                ans[p]= arr[j];
+                j++;
+                p++;
+            }
+        }
+        while(i<=mid){
+            ans[p] = arr[i];
+            i++;
+            p++;
+        }
+        while(j<=r){
+            ans[p] = arr[j];
+            j++;
+            p++;
+        }
+        
+        for(int i=0;i<r-l+1;i++){
+            arr[l+i] = ans[i];
+        }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l<r){
+            int mid = (l+r)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
