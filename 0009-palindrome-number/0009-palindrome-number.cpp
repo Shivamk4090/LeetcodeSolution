0fd00b6f581007b97105d4118f9041fac47c2a7b
SHIVAM KUMAR <57912926+Shivamk4090@users.noreply.github.com>
2022-11-24 11:16:52 +0530

    Time: 12 ms (92.56%), Space: 6.1 MB (10.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     
     bool isPalindrome(int x) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if(x<0) return false;
         string s = to_string(x);
         int i=0, j=s.size()-1;
