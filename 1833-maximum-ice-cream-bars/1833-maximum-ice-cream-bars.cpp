813e03a3fbc78c84c790d8a657f5da465ad5a502
SHIVAM KUMAR <57912926+Shivamk4090@users.noreply.github.com>
2023-01-06 12:32:20 +0530

    Time: 495 ms (9.50%), Space: 76.5 MB (76.68%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& v, int coins) {
-        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         sort(v.begin(), v.end());
         int ans = 0, i=0;
         while(coins>0 && i<v.size() && v[i]<=coins){
